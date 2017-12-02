263e0a4aedbbfeaee4afff55cd3493cff29d84da
musser_a <adrien.musserotte@gmail.com>
2017-12-02 10:46:03 +0100

    chore(project): initialize project with cordova

---
+++ kor_app/platforms/browser/www/js/app.js
@@ -0,0 +1,30 @@
+document.addEventListener('deviceready', function() {
+    navigator.splashscreen.hide();
+}, false);
+
+var app = angular.module('app', []);
+
+app.factory('GeolocationService', function($window, $q, $rootScope) {
+    var geolocation = $window.navigator.geolocation;
+    return {
+        getCurrentPosition: function(onSuccess, onError) {
+            geolocation.getCurrentPosition(function(position) {
+                $rootScope.$apply(function() {
+                    onSuccess(position);
+                })
+            }, function() {
+                $rootScope.$apply(function() {
+                    onError();
+                })
+            })
+        }
+    }
+})
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/home', { templateUrl: 'partials/home.html' })
+        .when('/about', { templateUrl: 'partials/about.html' })
+        .when('/game', { templateUrl: 'partials/game.html' })
+        .otherwise({ redirectTo: '/home' })
+})
